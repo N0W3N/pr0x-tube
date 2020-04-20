70ee45d35b93df2a61dc79f61247f817a55ea903
riccardo <15861569+N0W3N@users.noreply.github.com>
2020-04-20 16:30:28 +0200

    Remove For Loop for retrieving streaming link (parser.py) -> each streaming hoster gets its own variable, where soup.select can grab the links easier -> also for debugging causes if one link isn't available etc.
    Fix content processing via JSON API (posts-api.py)
    -> in order to make the api via JSON workable, the needed content information will be provided with `''.join` instead of loading them directly out of the list
    -> this will resolve the problem where it was impossible to send content data through our API when it contained special characters

--- parser.py
+++ parser.py
@@ -81,11 +81,10 @@ def content():
                     for image_link in url_soup.select('.aligncenter'):
                         img_link = image_link
                         # img_link = image_link['src']
-                    for download_link in url_soup.select(
-                            '.entry-content > p:nth-child(3) > strong:nth-child(1) > span:nth-'
-                            'child(1) > span:nth-child(1) > a:nth-child(2)'):
-                        dl_link = download_link
-                        # dl_link = download_link['href']
+                    go_link = url_soup.select(
+                            '.entry-content > p:nth-child(3) > strong:nth-child(1) > span:nth-child(1) > span:nth-child(1) > a:nth-child(2)')
+                    for dl_link in go_link:
+                        link = dl_link['href']
                     cat_temp = re.sub('Posted in', '', category.text)
                     cat_temp1 = re.sub('Tagged', '', cat_temp)
                     categories = re.sub('Bookmark the permalink', '', cat_temp1)
@@ -95,7 +94,7 @@ def content():
                     info_writer.writerow(([description]))
                     info_writer.writerow(([categories]))
                     info_writer.writerow(([img_link]))
-                    info_writer.writerow(([dl_link]))
+                    info_writer.writerow(([link]))
 
 
 def main():
